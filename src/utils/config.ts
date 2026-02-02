4a1279efbeeb072e809cdaeaf0d75f94bfc027ba
OlivierRecher <recher.olivier@outlook.fr>
2026-02-02 11:36:29 +0100

    feat(): env file with fake secret

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,16 +5,16 @@ import { configType } from '../types/config';
 dotenv.config();
 
 const config: configType = {
-  PORT: Number(process.env.PORT ?? 3002),
+  PORT: Number(process.env.PORT ?? 3009),
   API_GATEWAY_URL: process.env.API_GATEWAY_URL ?? 'http://localhost:3001',
   DATABASE_URL: `mysql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}:${Number(process.env.DATABASE_PORT)}/${process.env.DATABASE_DB_NAME}`,
   DATABASE_HOST: process.env.DATABASE_HOST ?? 'localhost',
   DATABASE_USER: process.env.DATABASE_USER ?? 'user',
   DATABASE_PASSWORD: process.env.DATABASE_PASSWORD ?? '',
-  DATABASE_PORT: Number(process.env.DATABASE_PORT ?? 3306),
+  DATABASE_PORT: Number(process.env.DATABASE_PORT ?? 3309),
   DATABASE_DB_NAME: process.env.DATABASE_DB_NAME ?? 'ram-ms-user-db',
 
-  SALT_ROUNDS: process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 100,
+  SALT_ROUNDS: process.env.SALT_ROUNDS ? Number(process.env.SALT_ROUNDS) : 10,
   JWT_SECRET: process.env.JWT_SECRET ?? 'ram-user-secret-jwt-token',
   INTERNAL_SECRET: process.env.INTERNAL_SECRET ?? 'internal_secret',
 };
