7d32cd821d4212c7439dc53ab4aac53e1de5cc6b
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-02-03 13:14:21 +0100

    fix docker #2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-echo "Waiting for database to be ready..."
 sleep 5
 
-echo "Running Prisma migrations..."
 npx prisma migrate deploy
 
-echo "Starting application..."
 exec tsx src/app.ts
