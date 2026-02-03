011bea4a82ffa9a33114827360897632f64a9264
Hugo CREPIN <crepin.hugo30@gmail.com>
2026-02-03 21:35:33 +0100

    openapi.yml

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ import { UserController } from './controllers/UserController';
 import { UserRouter } from './routes/UserRouter';
 import { UserService } from './services/UserService';
 import { config } from './utils/config';
+import fs from 'node:fs';
+import * as YAML from 'yaml';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 const port = config.PORT;
@@ -25,7 +28,13 @@ const userController = new UserController(userService);
 
 app.use('/user', new UserRouter(userController).router);
 
+const file = fs.readFileSync('./openapi.yml', 'utf8');
+const swaggerDocument = YAML.parse(file) as object;
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`docs available at http://localhost:${port}/docs`);
 });
